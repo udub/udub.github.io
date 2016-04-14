53be8c2d7f77ce3a535400502eac067085f78bda
udub <udub@users.noreply.github.com>
2016-04-14 05:45:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ alert("Welcome to the chat box!");
 function myFunction(){
   var textInput = document.getElementById("textbox");
   var comment = document.getElementById("comment");
-  comment.innerHTML = textInput.value;
+  var chatResult = "b3njamm1n >" + testInput.value;
+  comment.innerHTML = chatResult;
 }
